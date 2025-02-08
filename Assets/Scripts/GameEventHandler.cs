77b2730b6bbc8534a44a5dba23f57365564e9139
Alvin He <zhongjihe.alvin@gmail.com>
2025-02-08 12:07:12 -0800

    initialize git lfs

--- Assets/Scripts/GameEventHandler.cs
+++ Assets/Scripts/GameEventHandler.cs
@@ -1,24 +1,3 @@
-using UnityEngine;
-
-public class GameEventHandler : MonoBehaviour
-{
-    public string apiServerAddress = "127.0.0.1:8081";
-    public string cameraCaptureSaveFile = "./capture";
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        apiServerAddress = apiServerAddress.Trim();
-        cameraCaptureSaveFile = cameraCaptureSaveFile.Trim();
-    }
-
-    public void OnExit()
-    {
-        Debug.Log("exiting");
-        Application.Quit();
-    }
-
-    public void UpdateAPIServerAddress(string n) { apiServerAddress = n.Trim(); }
-    public void UpdateCameraCaptureSaveFile(string n) { cameraCaptureSaveFile = n.Trim(); }
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:805f5a7ace535dfb59cd1729e837ad1ffc46ed099c462a565eee986c5c4c5488
+size 720
